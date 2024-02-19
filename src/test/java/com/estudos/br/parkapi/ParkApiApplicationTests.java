126610a06283622b491792a06639769eec0c8a1a
Joelton <114880776+joeltonken@users.noreply.github.com>
2024-02-19 00:06:23 -0300

    iniciando projeto

---
+++ src/test/java/com/estudos/br/parkapi/ParkApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.estudos.br.parkapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParkApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
