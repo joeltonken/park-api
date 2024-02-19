1d3dfdde4787fb664bb92ee9b0c9c62a80a0c6a1
Joelton <114880776+joeltonken@users.noreply.github.com>
2024-02-19 15:17:37 -0300

    feat: findById Usuario

--- src/main/java/com/estudos/br/parkapi/service/UsuarioService.java
+++ src/main/java/com/estudos/br/parkapi/service/UsuarioService.java
@@ -16,4 +16,10 @@ public class UsuarioService {
     public Usuario salvar(Usuario usuario) {
         return usuarioRepository.save(usuario);
     }
+
+    @Transactional(readOnly = true)
+    public Usuario buscarPorId(Long id) {
+        return usuarioRepository.findById(id).orElseThrow(() -> new RuntimeException("Usuário não encontrado."));
+    }
+
 }
--- src/main/java/com/estudos/br/parkapi/web/controller/UsuarioController.java
+++ src/main/java/com/estudos/br/parkapi/web/controller/UsuarioController.java
@@ -5,10 +5,7 @@ import com.estudos.br.parkapi.service.UsuarioService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RequiredArgsConstructor
 @RestController
@@ -17,11 +14,16 @@ public class UsuarioController {
 
     private final UsuarioService usuarioService;
 
+    @GetMapping("/{id}")
+    public ResponseEntity<Usuario> getById(@PathVariable Long id) {
+        Usuario user = usuarioService.buscarPorId(id);
+        return ResponseEntity.ok(user);
+    }
+
     @PostMapping
     public ResponseEntity<Usuario> create(@RequestBody Usuario usuario) {
         Usuario user = usuarioService.salvar(usuario);
         return ResponseEntity.status(HttpStatus.CREATED).body(user);
     }
-
 
 }
