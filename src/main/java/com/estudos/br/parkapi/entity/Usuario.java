26a448c332f5612070ef5f261f6acb2874cdf5b0
Joelton <114880776+joeltonken@users.noreply.github.com>
2024-02-20 00:59:13 -0300

    refactor: usuario entity

--- src/main/java/com/estudos/br/parkapi/entity/Usuario.java
+++ src/main/java/com/estudos/br/parkapi/entity/Usuario.java
@@ -9,7 +9,9 @@ import java.io.Serializable;
 import java.time.LocalDateTime;
 import java.util.Objects;
 
-@Getter @Setter @NoArgsConstructor
+@Getter
+@Setter
+@NoArgsConstructor
 @Entity
 @Table(name = "usuarios")
 public class Usuario implements Serializable {
@@ -18,19 +20,26 @@ public class Usuario implements Serializable {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id")
     private Long id;
+
     @Column(name = "username", nullable = false, unique = true, length = 110)
     private String username;
+
     @Column(name = "password", nullable = false, length = 200)
     private String password;
+
     @Enumerated(EnumType.STRING)
     @Column(name = "role", nullable = false, length = 25)
-    private Role role;
+    private Role role = Role.ROLE_CLIENTE;
+
     @Column(name = "data_criacao")
     private LocalDateTime dataCriacao;
+
     @Column(name = "data_modificacao")
     private LocalDateTime dataModificacao;
+
     @Column(name = "criado_por")
     private String criadoPor;
+
     @Column(name = "modificado_por")
     private String modificadoPor;
 
@@ -54,7 +63,7 @@ public class Usuario implements Serializable {
     @Override
     public String toString() {
         return "Usuario{" +
-                "id=" + id +
+                "id= " + id +
                 '}';
     }
 }
