15834b7a99c6f9cb011062a26f116f9b2599b8ea
Joelton <114880776+joeltonken@users.noreply.github.com>
2024-02-27 16:29:34 -0300

    feat: add testes de buscar usuarios por id

--- src/test/java/com/estudos/br/parkapi/UsuarioIT.java
+++ src/test/java/com/estudos/br/parkapi/UsuarioIT.java
@@ -136,4 +136,34 @@ public class UsuarioIT {
         org.assertj.core.api.Assertions.assertThat(responseBody.getStatus()).isEqualTo(422);
     }
 
+    @Test
+    public void buscarUsuario_ComIdExistente_RetornarUsuarioComStatus200 () {
+        UsuarioResponseDTO responseBody = testClient
+                .get()
+                .uri("/api/v1/usuarios/100")
+                .exchange()
+                .expectStatus().isOk()
+                .expectBody(UsuarioResponseDTO.class)
+                .returnResult().getResponseBody();
+
+        org.assertj.core.api.Assertions.assertThat(responseBody).isNotNull();
+        org.assertj.core.api.Assertions.assertThat(responseBody.getId()).isEqualTo(100);
+        org.assertj.core.api.Assertions.assertThat(responseBody.getUsername()).isEqualTo("joelton@email.com");
+        org.assertj.core.api.Assertions.assertThat(responseBody.getRole()).isEqualTo("ADMIN");
+    }
+
+    @Test
+    public void buscarUsuario_ComIdInexistente_RetornarErrorMessageComStatus404 () {
+        ErrorMessage responseBody = testClient
+                .get()
+                .uri("/api/v1/usuarios/1000")
+                .exchange()
+                .expectStatus().isNotFound()
+                .expectBody(ErrorMessage.class)
+                .returnResult().getResponseBody();
+
+        org.assertj.core.api.Assertions.assertThat(responseBody).isNotNull();
+        org.assertj.core.api.Assertions.assertThat(responseBody.getStatus()).isEqualTo(404);
+    }
+
 }
