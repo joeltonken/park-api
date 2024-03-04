787e2dc5a4b3303b3409eb9c84c62670cd8859c1
Joelton <114880776+joeltonken@users.noreply.github.com>
2024-03-03 23:38:22 -0300

    fix: correcao de bug ao utilizar o token

--- src/main/java/com/estudos/br/parkapi/jwt/JwtAuthorizationFilter.java
+++ src/main/java/com/estudos/br/parkapi/jwt/JwtAuthorizationFilter.java
@@ -24,8 +24,9 @@ public class JwtAuthorizationFilter extends OncePerRequestFilter {
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
 
         final String token = request.getHeader(JwtUtils.JWT_AUTHORIZATION);
-        if (token == null || token.startsWith(JwtUtils.JWT_BEARER)) {
-            log.info("JWT Token está nulo, vazioou não iniciado com 'Bearer '.");
+
+        if (token == null || !token.startsWith(JwtUtils.JWT_BEARER)) {
+            log.info("JWT Token está nulo, vazio ou não iniciado com 'Bearer '.");
             filterChain.doFilter(request, response);
             return;
         }
