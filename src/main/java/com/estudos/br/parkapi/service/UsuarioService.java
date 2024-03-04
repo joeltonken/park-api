5a5877ac3059936bbdec413328215795b89f3928
Joelton <114880776+joeltonken@users.noreply.github.com>
2024-03-03 22:58:51 -0300

    feat: criptografando senha ao salvar e editar

--- src/main/java/com/estudos/br/parkapi/service/UsuarioService.java
+++ src/main/java/com/estudos/br/parkapi/service/UsuarioService.java
@@ -6,6 +6,7 @@ import com.estudos.br.parkapi.exception.PasswordInvalidException;
 import com.estudos.br.parkapi.exception.UsernameUniqueViolationException;
 import com.estudos.br.parkapi.repository.UsuarioRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -16,11 +17,13 @@ import java.util.List;
 public class UsuarioService {
 
     private final UsuarioRepository usuarioRepository;
+    private final PasswordEncoder passwordEncoder;
+
 
     @Transactional
     public Usuario salvar(Usuario usuario) {
-
         try {
+            usuario.setPassword(passwordEncoder.encode(usuario.getPassword()));
             return usuarioRepository.save(usuario);
         } catch (org.springframework.dao.DataIntegrityViolationException ex) {
             throw new UsernameUniqueViolationException(String.format("Username {%s} já cadastrado!", usuario.getUsername()));
@@ -44,11 +47,11 @@ public class UsuarioService {
             throw new PasswordInvalidException("Senhas não coincidem.");
         }
         Usuario user = buscarPorId(id);
-        if (!user.getPassword().equals(senhaAtual)){
+        if (!passwordEncoder.matches(senhaAtual, user.getPassword())){
             throw new PasswordInvalidException("Sua senha não confere.");
         }
 
-        user.setPassword(novaSenha);
+        user.setPassword(passwordEncoder.encode(novaSenha));
         return user;
     }
 
