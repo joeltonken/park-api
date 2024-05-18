176e32b551dc8ebef80e2acbc2c70a4aaa211425
Joelton <114880776+joeltonken@users.noreply.github.com>
2024-05-17 23:09:11 -0300

    feat: Adiciona DTOs de estacionamento

---
+++ src/main/java/com/estudos/br/parkapi/web/dto/EstacionamentoResponseDTO.java
@@ -0,0 +1,37 @@
+package com.estudos.br.parkapi.web.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Getter @Setter
+@NoArgsConstructor @AllArgsConstructor
+public class EstacionamentoResponseDTO {
+
+    private String placa;
+
+    private String marca;
+
+    private String modelo;
+
+    private String cor;
+
+    private String clienteCpf;
+
+    private String recibo;
+
+    private LocalDateTime dataEntrada;
+
+    private LocalDateTime dataSaida;
+
+    private String vagaCodigo;
+
+    private BigDecimal valor;
+
+    private BigDecimal desconto;
+
+}
---
+++ src/main/java/com/estudos/br/parkapi/web/dto/mapper/ClienteVagaMapper.java
@@ -0,0 +1,22 @@
+package com.estudos.br.parkapi.web.dto.mapper;
+
+import com.estudos.br.parkapi.entity.Cliente;
+import com.estudos.br.parkapi.entity.ClienteVaga;
+import com.estudos.br.parkapi.web.dto.EstacionamentoResponseDTO;
+import com.estudos.br.parkapi.web.dto.EstacionamentoCreateDTO;
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+import org.modelmapper.ModelMapper;
+
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public class ClienteVagaMapper {
+
+    public static ClienteVaga toClienteVaga(EstacionamentoCreateDTO dto) {
+        return new ModelMapper().map(dto, ClienteVaga.class);
+    }
+
+    public static EstacionamentoResponseDTO toDTO (ClienteVaga cliente) {
+        return new ModelMapper().map(cliente, EstacionamentoResponseDTO.class);
+    }
+
+}
