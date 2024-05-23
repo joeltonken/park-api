54204e684bad275bad0c74e8b4c22cb680bf51fe
Joelton <114880776+joeltonken@users.noreply.github.com>
2024-05-22 22:39:16 -0300

    feat: Adiciona documentacao swagger de VagaController

--- src/main/java/com/estudos/br/parkapi/web/controller/VagaController.java
+++ src/main/java/com/estudos/br/parkapi/web/controller/VagaController.java
@@ -5,9 +5,17 @@ import com.estudos.br.parkapi.service.VagaService;
 import com.estudos.br.parkapi.web.dto.VagaCreateDTO;
 import com.estudos.br.parkapi.web.dto.VagaResponseDTO;
 import com.estudos.br.parkapi.web.dto.mapper.VagaMapper;
+import com.estudos.br.parkapi.web.exception.ErrorMessage;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.headers.Header;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
@@ -23,13 +31,30 @@ public class VagaController {
 
     private final VagaService vagaService;
 
-    @PostMapping
+    @Operation(summary = "Recuperar uma vaga", description = "Recurso para localizar uma vaga pelo seu código." + "Requisição exige uso de um bearer token.",
+            security = @SecurityRequirement(name = "security"),
+            responses = {
+                    @ApiResponse(responseCode = "200", description = "Recurso recuperado com sucesso",
+                            content = @Content(mediaType = "application/json;charset=UTF-8", schema = @Schema(implementation = VagaResponseDTO.class))),
+                    @ApiResponse(responseCode = "404", description = "Vaga não encontrada",
+                            content = @Content(mediaType = "application/json;charset=UTF-8", schema = @Schema(implementation = ErrorMessage.class)))
+            })
+    @GetMapping("/{codigo}")
     @PreAuthorize("hasRole('ADMIN')")
     public ResponseEntity<VagaResponseDTO> getByCodigo(@PathVariable String codigo) {
         Vaga vaga = vagaService.buscarPorCodigo(codigo);
         return ResponseEntity.ok(VagaMapper.toDto(vaga));
     }
 
+    @Operation(summary = "Criar uma nova vaga", description = "Recurso para criar uma nova vaga",
+            responses = {
+                    @ApiResponse(responseCode = "201", description = "Recurso criado com sucesso",
+                            headers = @Header(name = HttpHeaders.LOCATION, description = "URL do recurso criado")),
+                    @ApiResponse(responseCode = "409", description = "Vaga já cadastrada no sistema",
+                            content = @Content(mediaType = "application/json;charset=UTF-8", schema = @Schema(implementation = ErrorMessage.class))),
+                    @ApiResponse(responseCode = "422", description = "Recurso não processado por dados de entrada inválidos",
+                            content = @Content(mediaType = "application/json;charset=UTF-8", schema = @Schema(implementation = ErrorMessage.class)))
+            })
     @PostMapping
     @PreAuthorize("hasRole('ADMIN')")
     public ResponseEntity<Void> create(@RequestBody @Valid VagaCreateDTO dto) {
