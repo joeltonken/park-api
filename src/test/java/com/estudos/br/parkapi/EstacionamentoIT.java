a429fb74b4e5c8f444ccf2988cc32d0f6a98e205
Joelton <114880776+joeltonken@users.noreply.github.com>
2024-05-27 23:15:53 -0300

    test: Adiciona teste de checkin com status 403 e 422

--- src/test/java/com/estudos/br/parkapi/EstacionamentoIT.java
+++ src/test/java/com/estudos/br/parkapi/EstacionamentoIT.java
@@ -25,7 +25,7 @@ public class EstacionamentoIT {
                 .cor("AZUL").clienteCpf("09191773016")
                 .build();
 
-        testClient.post().uri("/api/v1/estacionamentos/check-in")
+        testClient.post().uri("/estacionamentos/check-in")
                 .contentType(MediaType.APPLICATION_JSON)
                 .headers(JwtAuthentication.getHeaderAuthentication(testClient, "ana@email.com.br", "123456"))
                 .bodyValue(createDTO)
@@ -42,6 +42,44 @@ public class EstacionamentoIT {
                 .jsonPath("dataEntrada").exists()
                 .jsonPath("vagaCodigo").exists();
 
+    }
+
+    @Test
+    public void criarCheckin_ComRoleCliente_RetornarErrorStatus403() {
+        EstacionamentoCreateDTO createDTO = EstacionamentoCreateDTO.builder()
+                .placa("WER-2024").marca("FIAT").modelo("PALIO 1.0")
+                .cor("AZUL").clienteCpf("09191773016")
+                .build();
+
+        testClient.post().uri("/estacionamentos/check-in")
+                .contentType(MediaType.APPLICATION_JSON)
+                .headers(JwtAuthentication.getHeaderAuthentication(testClient, "bia@email.com.br", "123456"))
+                .bodyValue(createDTO)
+                .exchange()
+                .expectStatus().isForbidden()
+                .expectBody()
+                .jsonPath("status").isEqualTo("403")
+                .jsonPath("path").isEqualTo("/estacionamentos/check-in")
+                .jsonPath("method").isEqualTo("POST");
+    }
+
+    @Test
+    public void criarCheckin_ComDadosInvalidos_RetornarErrorStatus422() {
+        EstacionamentoCreateDTO createDTO = EstacionamentoCreateDTO.builder()
+                .placa("").marca("").modelo("")
+                .cor("").clienteCpf("")
+                .build();
+
+        testClient.post().uri("/estacionamentos/check-in")
+                .contentType(MediaType.APPLICATION_JSON)
+                .headers(JwtAuthentication.getHeaderAuthentication(testClient, "bia@email.com.br", "123456"))
+                .bodyValue(createDTO)
+                .exchange()
+                .expectStatus().isEqualTo(422)
+                .expectBody()
+                .jsonPath("status").isEqualTo("422")
+                .jsonPath("path").isEqualTo("/estacionamentos/check-in")
+                .jsonPath("method").isEqualTo("POST");
     }
 
 }
