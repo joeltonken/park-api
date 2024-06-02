da915df12b8541b0d5403c4e112ddfc7dcab4ad7
Joelton <114880776+joeltonken@users.noreply.github.com>
2024-06-01 23:27:49 -0300

    test: Adiciona testes com endpoint de checkout com status 200, 403 e 404

--- src/test/java/com/estudos/br/parkapi/EstacionamentoIT.java
+++ src/test/java/com/estudos/br/parkapi/EstacionamentoIT.java
@@ -174,4 +174,56 @@ public class EstacionamentoIT {
                 .jsonPath("method").isEqualTo("GET");
     }
 
+    @Test
+    public void criarCheckout_ComReciboExistente_RetornarStatusSucesso() {
+
+        testClient.put()
+                .uri("/estacionamentos/check-out/{recibo}", "20240313-101300")
+                .headers(JwtAuthentication.getHeaderAuthentication(testClient, "ana@email.com.br", "123456"))
+                .exchange()
+                .expectStatus().isOk()
+                .expectBody()
+                .jsonPath("placa").isEqualTo("FIT-1020")
+                .jsonPath("marca").isEqualTo("FIAT")
+                .jsonPath("modelo").isEqualTo("PALIO")
+                .jsonPath("cor").isEqualTo("VERDE")
+                .jsonPath("clienteCpf").isEqualTo("98401203015")
+                .jsonPath("recibo").isEqualTo("20240313-101300")
+                .jsonPath("dataEntrada").isEqualTo("2024-03-13 10:15:00")
+                .jsonPath("vagaCodigo").isEqualTo("A-01")
+                .jsonPath("dataSaida").exists()
+                .jsonPath("valor").exists()
+                .jsonPath("desconto").exists();
+    }
+
+    @Test
+    public void criarCheckout_ComRoleCliente_RetornarStatusError403() {
+
+        testClient.put()
+                .uri("/estacionamentos/check-out/{recibo}", "20240313-101300")
+                .headers(JwtAuthentication.getHeaderAuthentication(testClient, "bia@email.com.br", "123456"))
+                .exchange()
+                .expectStatus().isForbidden()
+                .expectBody()
+                .jsonPath("status").isEqualTo("403")
+                .jsonPath("path").isEqualTo("/estacionamentos/check-out/20240313-101300")
+                .jsonPath("method").isEqualTo("PUT");
+
+    }
+
+    @Test
+    public void criarCheckout_ComReciboInexistente_RetornarStatusError404() {
+
+        testClient.put()
+                .uri("/estacionamentos/check-out/{recibo}", "20240313-000000")
+                .headers(JwtAuthentication.getHeaderAuthentication(testClient, "ana@email.com.br", "123456"))
+                .exchange()
+                .expectStatus().isNotFound()
+                .expectBody()
+                .jsonPath("status").isEqualTo("404")
+                .jsonPath("path").isEqualTo("/estacionamentos/check-out/20240313-000000")
+                .jsonPath("method").isEqualTo("PUT");
+
+    }
+
 }
